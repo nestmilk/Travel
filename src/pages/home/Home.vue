79ebf07e28cc2694b562e4d1db371ddb85dc7877
gong zhenhua <nestmilk@live.com>
2019-04-08 18:53:09 +0800

    try

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,7 +10,7 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
-
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
